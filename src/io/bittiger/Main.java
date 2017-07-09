c718853033b82911e9ac75d99f2fa63d3eeaab51
YuChiTao <634439570@qq.com>
2017-07-09 16:25:33 +0800

    可以指定要生成索引的数据

--- src/io/bittiger/Main.java
+++ src/io/bittiger/Main.java
@@ -2,16 +2,23 @@ package io.bittiger;
 
 public class Main {
 
+    /**
+     * 0,Ad数据
+     * 1,budget数据
+     * 2,server & 3,port
+     * MySQL 4,server:port, 5,database name, 6,user, 7,password
+     */
     public static void main(String[] args) {
+
         AdsEngine adsEngine = new AdsEngine(
-                "/EclipseWorkspace/SearchAdsDemo/data/ads_0502.txt",
-                "/EclipseWorkspace/SearchAdsDemo/data/budget.txt",
-                "127.0.0.1",
-                11211,
-                "127.0.0.1:3306",
-                "searchads",
-                "root",
-                "password"
+                "./" + args[0],//"./ads_data1.txt"**
+                "./" + args[1],//"./budget.txt"
+                args[2],//"127.0.0.1"
+                Integer.parseInt(args[3]),//11211
+                args[4],//"127.0.0.1:3306"
+                args[5],//"searchads"
+                args[6],//"root"
+                args[7]//"password"
         );
         adsEngine.init();
     }
